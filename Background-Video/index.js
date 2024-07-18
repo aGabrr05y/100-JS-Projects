c82873a242a9c33b959119f967fe69de30d4125d
Antonio Ramirez <agabrielramirez09@gmail.com>
2024-07-17 23:48:12 -0600

    I just finish another proyect

---
+++ Background-Video/index.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    const video = document.getElementById("background-video")
+    const buttom = document.getElementById ("buttom")
+
+    buttom.addEventListener ("click",() =>{
+        if (video.paused) {
+            video.play();
+            buttom.textContent = '⏸️';
+        } else {
+            video.pause();
+            buttom.textContent = '▶️';
+        }
+
+})})
